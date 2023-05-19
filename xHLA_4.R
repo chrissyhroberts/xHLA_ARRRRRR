033150820d1c61c5b692ac2924e84da3c3c1c167
chrissyhroberts <chrissyhroberts@yahoo.co.uk>
2023-05-19 12:00:39 +0100

    Bug fixes
    * Fixes an error where the system failed if only one pair of Fastq files was provided
    * Fixes an error where the report failed to create when there was no representation of sequences for any of the six HLA genes. This occurred sometimes with low quality samples
    * Adds better report
    * Adds some useful messages to signpost progress

--- xHLA_4.R
+++ xHLA_4.R
@@ -43,7 +43,7 @@ system("chmod +x bin/*")
 
 xhla<-function(sample, reads.to.use,seeder = sample(1:1e6, 1)){
 
-
+  message("###################### Starting Analysis on  ",sample," ######################")
   # calculate the file names
 
   readone<-paste(sample,"_L001_R1_001.fastq",sep="")
@@ -58,25 +58,39 @@ xhla<-function(sample, reads.to.use,seeder = sample(1:1e6, 1)){
   message("Running bwa to compare chr6 to fastq")
   system(paste("bwa mem chr6/chr6.fa input/input_tmp1.fastq input/input_tmp2.fastq > input/",sample,".sam",sep=""))
 
-  message("Running samtools to index and align reads")
+  message("Running samtools to index align reads")
 
   system(paste("samtools view -bT chr6/chr6.fa input/",sample,".sam > input/",sample,".bam",sep=""))
+  
+  message("Running samtools to sort")
+
   system(paste("samtools sort input/",sample,".bam > input/",sample,".sorted.bam",sep=""))
+  
+  message("Running samtools to index")
+  
   system(paste("samtools index input/",sample,".sorted.bam input/",sample,".sorted.bai",sep=""))
+
+  message("Running samtools to extract HLA region")
 
   system(paste("samtools view -u input/",sample,".sorted.bam chr6:29886751-33090696 | samtools view -L ./data/hla.bed - > input/",sample,"_extract.sam",sep=""))
 
+  message("Running preprocess.pl script to deduplicate, quality filter and trim sequences")
+
   system(paste("perl bin/preprocess.pl input/",sample,"_extract.sam | gzip > input/",sample,".fq.gz",sep=""))
 
-  #Aligning reads to IMGT database
+  message("Running align.pl script to align reads to IMGT database")
+  
   system(paste("bin/align.pl input/",sample,".fq.gz input/",sample,".tsv",sep=""))
 
-  #performing typing
+  message ("Running typing.r to perform HLA typing")
+  
   system(paste("bin/typing.r input/",sample,".tsv input/",sample,".hla",sep=""))
 
-  #echo "$BIN/report.py -in $OUT/${ID}.hla -out $OUT/${ID}.json -subject $ID -sample $ID"
+  message ("Running report.py to convert .hla file to .json report")
 
   system(paste("python3 bin/report.py -in input/",sample,".hla -out input/",sample,".json -subject ",sample," -sample ",sample,sep=""))
+
+  message ("Running full.r to convert .hla file to .json report")
 
   system(paste("bin/full.r input/",sample,".tsv input/",sample,".hla input/",sample,".hla.full",sep=""))
 
@@ -91,25 +105,23 @@ xhla<-function(sample, reads.to.use,seeder = sample(1:1e6, 1)){
   system("rm input/*.tsv")
 
 
+  message("########################## Writing report ########################## ")
 
   result = read_json(paste("input/",sample,".json",sep="")) %>%
     as_tibble
 
-  result.report<-tibble(
-    sample = result$subject_id,
-    HLA_Ai = result$hla$alleles[[1]],
-    HLA_Aii = result$hla$alleles[[2]],
-    HLA_Bi = result$hla$alleles[[3]],
-    HLA_Bii = result$hla$alleles[[4]],
-    HLA_Ci = result$hla$alleles[[5]],
-    HLA_Cii = result$hla$alleles[[6]],
-    HLA_DRB1i = result$hla$alleles[[7]],
-    HLA_DRB1ii = result$hla$alleles[[8]],
-    HLA_DQB1i = result$hla$alleles[[9]],
-    HLA_DQB1ii = result$hla$alleles[[10]],
-    HLA_DPB1i = result$hla$alleles[[11]],
-    HLA_DPB1ii = result$hla$alleles[[12]]
-  )
+result.report = tibble (allele = fromJSON(txt = paste("input/",sample,".json",sep=""))$hla$alleles) %>% 
+        mutate (gene   = str_split(allele,pattern = "\\*",simplify = T)[,1],
+                allele = str_split(allele,pattern = "\\*",simplify = T)[,2],
+                gene = case_when(
+                                duplicated(gene) == FALSE ~ str_c(gene,"i"),
+                                duplicated(gene) == TRUE ~ str_c(gene,"ii")
+                                )
+                ) %>% 
+        left_join(tibble (gene = c("Ai","Aii","Bi","Bii","Ci","Cii","DRB1i","DRB1ii","DQB1i","DQB1ii","DPB1i","DPB1ii")),.,multiple = "all") %>% 
+        mutate(sample_id = fromJSON(txt = paste("input/",sample,".json",sep=""))$sample_id) %>% 
+        pivot_wider(id_cols = sample_id,names_from = gene,values_from = allele )
+
 
   write_csv(result.report,file = paste("output/",sample,"_hla_typing_results.csv",sep=""))
   return(result.report)
@@ -138,9 +150,12 @@ df<-xhla(sample = samples$id[1],reads.to.use = 7500)
 ## Loop on remaining samples, binding each to df and saving individual reports
 ##########################################################################
 
+if(nrow(samples)>1){
 for(i in 2:nrow(samples)){
                           message(paste("starting alignment",i,"of ",nrow(samples),sep = " "))
                           df<-bind_rows(df,xhla(sample = samples$id[i],reads.to.use = 7500))
                           }
-
+					}
 write_csv(df,"output/full_typing_report.csv")
+
+
